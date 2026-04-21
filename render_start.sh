f19666c7579613ad433ed7719cc7093d4bce881d
Udhay Singh Marwah <148549473+udhay2004@users.noreply.github.com>
2026-04-21 08:56:49 +0530

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-apt-get install -y ffmpeg 2>/dev/null || true
-cd /opt/render/project/src
-exec gunicorn app:app --workers=1 --timeout=300 --bind=0.0.0.0:$PORT
+exec gunicorn app:app --workers=1 --timeout=300 --bind=0.0.0.0:$PORT --chdir /opt/render/project/src
